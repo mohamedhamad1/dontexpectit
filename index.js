6560f733ccb7cf455de43585085cebc2f9c5eda3
NfSwilam01 <66533353+mohamedhamad1@users.noreply.github.com>
2024-09-03 18:46:10 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ mongoose
     .then(()=>{console.log('db connect successfully');})
     .catch((err)=>{console.log(err);})
 const Flag = require('./model')
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
+const { name } = require('ejs');
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -33,7 +34,13 @@ app.post('/',async(req, res)=>{
     }
 })
 app.get('/evil',(req,res)=>{
-    res.send('Farmer{0k, U are H2cker nOw, St0p FUZZ Me}')
+    const flag = req.query.flag || null
+    const isAuth = flag == "dGhpcyBpcyBteSBmbGFnIHRva2VuIHRvIHZhbGlkYXRlIHVzZXIgYXV0aG9yaXphdGlvbg=="
+    if(isAuth){
+        res.status(200).render("evil",{flag:"Farmer{0oh Br0, U aRe R3al Farmer}"})
+    }else{
+        res.status(403).render("evil",{flag:"Code:403 Forbidden, you are unauthorized to rech that"})
+    }
 })
 app.all('*',(req, res)=>{
     res.status(404).send('<h1>Congratulation you found the FLAG...<br>Hhhh Sorry i kidding you, 404 PageNotFound<br>Go to where you came \.\')</h1>')
---
+++ public/flagValidate.js
@@ -0,0 +1,3 @@
+function validation(token){
+    return ((token === "dGhpcyBpcyBteSBmbGFnIHRva2VuIHRvIHZhbGlkYXRlIHVzZXIgYXV0aG9yaXphdGlvbg==")?true:false)
+}
